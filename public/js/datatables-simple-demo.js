19b6765c325764d2b5d5c615af773de1f818c4b4
urooj <uroojnishat11@gmail.com>
2026-06-20 01:53:55 +0500

    Add homepage background image and retain datatable page after product edit

--- public/js/datatables-simple-demo.js
+++ public/js/datatables-simple-demo.js
@@ -1,11 +1,17 @@
 let myTable
 window.addEventListener('DOMContentLoaded', event => {
-    // Simple-DataTables
-    // https://github.com/fiduswriter/Simple-DataTables/wiki
-
     const datatablesSimple = document.getElementById('datatablesSimple');
     if (datatablesSimple) {
         myTable = new simpleDatatables.DataTable(datatablesSimple);
+        
+        myTable.on('datatable.init', function() {
+            let savedPage = sessionStorage.getItem('dt_saved_page');
+            if (savedPage) {
+                setTimeout(() => {
+                    myTable.page(parseInt(savedPage));
+                    sessionStorage.removeItem('dt_saved_page');
+                }, 50);
+            }
+        });
     }
-
 });
